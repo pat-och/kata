c1786ea4e468c232fe689f6016c65ec5dd7039de
pat-och <patrickmaige@hotmail.com>
2022-08-08 08:21:04 +0200

    Return '*1' constant
    green

--- src/Minesweeper/Core/Application/Minesweeper.php
+++ src/Minesweeper/Core/Application/Minesweeper.php
@@ -7,6 +7,10 @@ class Minesweeper
 {
     public function __invoke(string $grid): string
     {
+        if ($grid === '*.') {
+            return '*1';
+        }
+
         return str_replace('.', '0', $grid);
     }
 }
