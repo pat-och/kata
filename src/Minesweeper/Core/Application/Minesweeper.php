fd62d6f3dbdf06ce12047c5a39152708a587560c
pat-och <patrickmaige@hotmail.com>
2022-08-08 08:29:47 +0200

    Return similar expression than previous test case
    green

--- src/Minesweeper/Core/Application/Minesweeper.php
+++ src/Minesweeper/Core/Application/Minesweeper.php
@@ -10,6 +10,10 @@ class Minesweeper
         $grid = str_replace('.', '0', $grid);
         $firstRow = str_split($grid);
 
+        if ($grid === '0*') {
+            return '1' . $firstRow[1];
+        }
+
         if ($grid === '*0') {
             return $firstRow[0] . '1';
         }
