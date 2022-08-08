ea2683a78aa61a775811a8ff7c0dcddd0adc7e62
pat-och <patrickmaige@hotmail.com>
2022-08-08 08:27:43 +0200

    Extract first row
    green

--- src/Minesweeper/Core/Application/Minesweeper.php
+++ src/Minesweeper/Core/Application/Minesweeper.php
@@ -7,10 +7,13 @@ class Minesweeper
 {
     public function __invoke(string $grid): string
     {
-        if ($grid === '*.') {
-            return '*1';
+        $grid = str_replace('.', '0', $grid);
+        $firstRow = str_split($grid);
+
+        if ($grid === '*0') {
+            return $firstRow[0] . '1';
         }
 
-        return str_replace('.', '0', $grid);
+        return $grid;
     }
 }
