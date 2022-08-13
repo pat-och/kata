2c59497742b6373e80e7e3c1fb71c30aa4ee1896
pat-och <patrickmaige@hotmail.com>
2022-08-13 06:54:18 +0200

    Generalize usage of board->hasMineAtRight
    green

--- src/Minesweeper/Core/Application/Minesweeper.php
+++ src/Minesweeper/Core/Application/Minesweeper.php
@@ -51,7 +51,7 @@ class Minesweeper
         if ($this->stringSchemeGrid === '00*') {
             return sprintf(
                 '%s%s%s',
-                $this->board->cell(1)->isMine() ? $this->increaseCellContent(0) : $this->cells[0]->value,
+                $this->board->hasMineAtRight(0) ? $this->increaseCellContent(0) : $this->cells[0]->value,
                 $this->increaseCellContent(1),
                 $this->increaseCellContent(2)
             );
@@ -60,7 +60,7 @@ class Minesweeper
         if ($this->stringSchemeGrid === '0*0') {
             return sprintf(
                 '%s%s%s',
-                $this->board->cell(1)->isMine() ? $this->increaseCellContent(0) : $this->cells[0]->value,
+                $this->board->hasMineAtRight(0) ? $this->increaseCellContent(0) : $this->cells[0]->value,
                 $this->increaseCellContent(1),
                 $this->board->cell(1)->isMine() ? $this->increaseCellContent(2) : $this->cells[2]->value
             );
@@ -69,8 +69,8 @@ class Minesweeper
         if ($this->stringSchemeGrid === '**0') {
             return sprintf(
                 '%s%s%s',
-                $this->board->cell(1)->isMine() ? $this->increaseCellContent(0) : $this->cells[0]->value,
-                ($this->board->cell(0)->isMine() && $this->board->cell(2)->isMine()) ? 2 : $this->cells[1]->value,
+                $this->board->hasMineAtRight(0) ? $this->increaseCellContent(0) : $this->cells[0]->value,
+                ($this->board->cell(0)->isMine() && $this->board->hasMineAtRight(1)) ? 2 : $this->cells[1]->value,
                 $this->board->cell(1)->isMine() ? $this->increaseCellContent(2) : $this->cells[2]->value
             );
         }
@@ -78,8 +78,8 @@ class Minesweeper
         if ($this->stringSchemeGrid === '0**') {
             return sprintf(
                 '%s%s%s',
-                $this->board->cell(1)->isMine() ? $this->increaseCellContent(0) : $this->cells[0]->value,
-                ($this->board->cell(0)->isMine() && $this->board->cell(2)->isMine()) ? 2 : $this->cells[1]->value,
+                $this->board->hasMineAtRight(0) ? $this->increaseCellContent(0) : $this->cells[0]->value,
+                ($this->board->cell(0)->isMine() && $this->board->hasMineAtRight(1)) ? 2 : $this->cells[1]->value,
                 $this->board->cell(1)->isMine() ? $this->increaseCellContent(2) : $this->cells[2]->value
             );
         }
@@ -87,8 +87,8 @@ class Minesweeper
         if ($this->stringSchemeGrid === '*0*') {
             return sprintf(
                 '%s%s%s',
-                $this->board->cell(1)->isMine() ? $this->increaseCellContent(0) : $this->cells[0]->value,
-                ($this->board->cell(0)->isMine() && $this->board->cell(2)->isMine()) ? 2 : $this->cells[1]->value,
+                $this->board->hasMineAtRight(0) ? $this->increaseCellContent(0) : $this->cells[0]->value,
+                ($this->board->cell(0)->isMine() && $this->board->hasMineAtRight(1)) ? 2 : $this->cells[1]->value,
                 $this->board->cell(1)->isMine() ? $this->increaseCellContent(2) : $this->cells[2]->value
             );
         }
