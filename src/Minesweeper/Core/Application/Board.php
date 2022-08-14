175a465c462670f53582d973c903a7ee5a0e0e58
pat-och <patrickmaige@hotmail.com>
2022-08-14 21:27:39 +0200

    Extend row index
    green

--- src/Minesweeper/Core/Application/Board.php
+++ src/Minesweeper/Core/Application/Board.php
@@ -36,30 +36,32 @@ class Board
 
     private function resolve(): void
     {
-        for ($column = 0; $column < strlen($this->grid); ++$column) {
-            $this->increaseNearbyCellsIfCellIsMine($column);
+        foreach($this->rows as $row => $rowAsString) {
+            for ($column = 0; $column < strlen($this->grid); ++$column) {
+                $this->increaseNearbyCellsIfCellIsMine($row, $column);
+            }
         }
     }
 
-    private function increaseNearbyCellsIfCellIsMine(int $column): void
+    private function increaseNearbyCellsIfCellIsMine(int $row, int $column): void
     {
         if ($this->cells[$column]->isEmpty()) return;
-        $this->increaseLeftAndRightEmptyCells($column);
+        $this->increaseLeftAndRightEmptyCells($row, $column);
     }
 
-    private function increaseLeftAndRightEmptyCells(int $column): void
+    private function increaseLeftAndRightEmptyCells(int $row, int $column): void
     {
-        $this->increaseEmptyRightCell($column);
-        $this->increaseEmptyLeftCell($column);
+        $this->increaseEmptyRightCell($row, $column);
+        $this->increaseEmptyLeftCell($row, $column);
     }
 
-    private function increaseEmptyRightCell(int $column): void
+    private function increaseEmptyRightCell(int $row, int $column): void
     {
         if (array_key_exists($column + 1, $this->cells))
             $this->cells[$column + 1]->increase();
     }
 
-    private function increaseEmptyLeftCell(int $column): void
+    private function increaseEmptyLeftCell(int $row, int $column): void
     {
         if (array_key_exists($column - 1, $this->cells))
             $this->cells[$column - 1]->increase();
